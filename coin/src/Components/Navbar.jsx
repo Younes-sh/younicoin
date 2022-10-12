649507c1792e568925d0f35eeae2c898cfd8e8ff
younes <sheikhlaryounes@gmail.com>
2022-10-12 12:15:45 +0200

    change navbar

--- coin/src/Components/Navbar.jsx
+++ coin/src/Components/Navbar.jsx
@@ -17,7 +17,7 @@ const Navbar = () => {
           </li>
 
           <li>
-            <Link to="/about">About</Link>
+            <Link to="/about">NFT</Link>
           </li>
         </ul>
       </div>
