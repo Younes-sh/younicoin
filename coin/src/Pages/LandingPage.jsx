6967365074d8213d0d91eba9ca6257789d90d107
younes <sheikhlaryounes@gmail.com>
2022-10-13 11:50:03 +0200

    created landin page

---
+++ coin/src/Pages/LandingPage.jsx
@@ -0,0 +1,27 @@
+import React from  'react';
+
+const LandingPage = () => {
+    return (
+        <div className='LandingPage'>
+                <div className='containerLandingPage'>
+                    <div>
+                        <div className='section1'>
+
+                        </div>
+                    </div>
+                    {/* -------------------- section 2 */}
+                    <div>
+                        <div className='section2'>
+
+                        </div>
+                    </div>
+                    {/* -------------------- section 3 */}
+                    <div>
+                        <div className='section3'>
+
+                        </div>
+                    </div>
+                </div>
+        </div>
+    )
+}
