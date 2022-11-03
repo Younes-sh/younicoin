f3ce11987b85a4dacbd75d93b95efc732a639fbb
younes sheikhlar <sheikhlaryounes@mail.com>
2022-11-04 00:25:46 +0100

    update

--- coin/src/Components/ElementHomePage/Section_4.jsx
+++ coin/src/Components/ElementHomePage/Section_4.jsx
@@ -1,18 +1,54 @@
 import React from 'react';
 import styled from '@emotion/styled';
-
-
+import secure from '../../asset/protection.png';
 const Div = styled.div`
 width:70%;
 height:700px;
-padding:50px;
-background-color:gray;
+margin-bottom:50px;
+display:flex;
+// background-color:gray;
+`;
+
+const ContainerImage = styled.div`
+    width:50%;
+    height:auto;
+    // background-color:#102626; 
+    position:relative;
+
+    
+`;
+
+const Rotation = styled.div`
+width:100%;
+height:100%;
+display:flex;
+position:absolute;
+justify-content:center;
+align-items:center;
+border-radius:50%;
+
+`;
+const Img = styled.img`
+    width:100%;
+`;
+const ContainerTex = styled.div`
+
 `;
 
 const Section_4 = () => {
     return (
         <Div>
-            <h1>younes sheikhlar 4</h1>
+            <ContainerImage>
+                    <Rotation className='Rotation'>
+                        <Img className='image1' src={secure} alt='NFT1'/> 
+                    </Rotation>
+                <div style={{width:'100%',height:'100%',zIndex:9}}>
+                    
+                </div>
+            </ContainerImage>
+            <ContainerTex>
+                <div></div>
+            </ContainerTex>
         </Div>
     )
 }
