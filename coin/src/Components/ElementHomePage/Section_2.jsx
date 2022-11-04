c0449800fa2ba74e968b43af7a70a326d11adc2c
younes sheikhlar <sheikhlaryounes@mail.com>
2022-11-04 22:33:02 +0100

    Hover boxe-shadow in section_2

--- coin/src/Components/ElementHomePage/Section_2.jsx
+++ coin/src/Components/ElementHomePage/Section_2.jsx
@@ -23,17 +23,15 @@ const Div = styled.div`
     const Elements = styled.div`
         min-width:30px;
         height:200px;
-        // margin-bottom:10px ;
         margin: 10px 5px;
         padding:20px;
         display:flex;
         flex-direction:column;
-        position: relative;
-
+        border-radius:5px;
         // -----------------
         box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.8), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
         :hover {
-            border:solid 1px gold;
+            box-shadow: 0 4px 8px 0 rgba(70, 150, 131, 0.8), 0 6px 20px 0 rgba(0, 0, 20, 0.19);
         }
     `;
 const Child = styled.div`
