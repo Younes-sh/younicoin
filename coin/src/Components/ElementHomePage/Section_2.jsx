ec8e63fca90d7c978b97901286cd911f06df63c7
younes sheikhlar <sheikhlaryounes@mail.com>
2022-11-06 13:47:35 +0100

    modified section 2

--- coin/src/Components/ElementHomePage/Section_2.jsx
+++ coin/src/Components/ElementHomePage/Section_2.jsx
@@ -34,9 +34,6 @@ const Div = styled.div`
             box-shadow: 0 4px 8px 0 rgba(70, 150, 131, 0.8), 0 6px 20px 0 rgba(0, 0, 20, 0.19);
         }
     `;
-const Child = styled.div`
-  
-`;
 const Section_2 = () => {
 
     return (
